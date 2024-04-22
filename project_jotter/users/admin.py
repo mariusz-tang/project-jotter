7c3201808fda64d348c75090672d041cb802f735
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-04-22 20:03:16 +0100

    Create user model

--- project_jotter/users/admin.py
+++ project_jotter/users/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Overriden to reflect field mismatches between the default User
+    model and the one defined in this application.
+    """
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("given_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_verified",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+    # ! These may need overriding when the relevant custom forms are created
+    # form = UserChangeForm
+    # add_form = UserCreationForm
+
+    list_display = ("username", "email", "given_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_verified", "groups")
+    search_fields = ("username", "given_name", "email")
+
+
+admin.site.register(User, UserAdmin)
--- project_jotter/users/models.py
+++ project_jotter/users/models.py
@@ -1,3 +1,112 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.core.mail import send_mail
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a user with the given username, email and
+        password.
+        """
+        if not username:
+            raise ValueError("Users must have a username")
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        """
+        Creates and saves a staff superuser with the given username, email
+        and password.
+        """
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_verified = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    The User model. Implements admin-compliant permissions.
+
+    Username, email, and password are required. Other fields are optional.
+    """
+
+    username_validator = RegexValidator(
+        regex=r"^[\w.@+-]+\Z",
+        message=(
+            "Please enter a valid username. This value may contain only letters, "
+            "numbers, and @/./+/-/_ characters."
+        ),
+    )
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. Your username must be at most 150 charactes long "
+            "and consist solely of letters, numbers and @/./+/-/_."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField(
+        "email address",
+        unique=True,
+        help_text="Required.",
+        error_messages={
+            "unique": "A user with that email address already exists.",
+        },
+    )
+
+    given_name = models.CharField("preferred name", max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into the admin site.",
+    )
+    is_verified = models.BooleanField(
+        "email verification status",
+        default=False,
+        help_text="Designates whether the user has verified their email address.",
+    )
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_short_name(self):
+        return self.given_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
