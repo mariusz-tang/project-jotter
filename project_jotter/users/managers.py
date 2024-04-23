0ed8660c6557aafb6f63d1fb402232630b3367a9
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-04-23 16:46:13 +0100

    Move user manager to users/managers.py

---
+++ project_jotter/users/managers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a user with the given username, email and
+        password.
+        """
+        if not username:
+            raise ValueError("Users must have a username")
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        """
+        Creates and saves a staff superuser with the given username, email
+        and password.
+        """
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_verified = True
+        user.save(using=self._db)
+        return user
--- project_jotter/users/models.py
+++ project_jotter/users/models.py
@@ -1,49 +1,10 @@
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    BaseUserManager,
-    PermissionsMixin,
-)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.core.mail import send_mail
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
 
-
-class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
-        """
-        Creates and saves a user with the given username, email and
-        password.
-        """
-        if not username:
-            raise ValueError("Users must have a username")
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None):
-        """
-        Creates and saves a staff superuser with the given username, email
-        and password.
-        """
-        user = self.create_user(
-            username=username,
-            email=email,
-            password=password,
-        )
-        user.is_superuser = True
-        user.is_staff = True
-        user.is_verified = True
-        user.save(using=self._db)
-        return user
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
