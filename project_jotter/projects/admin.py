187758405bc60fd545bafd410d1ad291f38898e2
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-04-30 19:53:38 +0100

    Add project model

--- project_jotter/projects/admin.py
+++ project_jotter/projects/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.admin import UserAdmin
+from .models import Project
+
+
+class ProjectInline(admin.StackedInline):
+    model = Project
+    extra = 0
+    show_change_link = True
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = ((None, {"fields": ("name", "author", "completed")}),)
+    list_display = ("name", "author", "completed")
+    list_filter = ("completed",)
+    search_fields = ("name", "author")
+
+
+UserAdmin.inlines = (*UserAdmin.inlines, ProjectInline)
+admin.site.register(Project, ProjectAdmin)
--- project_jotter/projects/models.py
+++ project_jotter/projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Project(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    name = models.CharField(max_length=150)
+    image = models.ImageField(blank=True)
+    contents = models.JSONField(blank=True, null=True)
+    completed = models.BooleanField(blank=True, default=False)
+
+    def __str__(self):
+        return f"{self.name} ({self.author})"
+
+    class Meta:
+        unique_together = (("author", "name"),)
