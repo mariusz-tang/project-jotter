8872840e60948dee6acc233556cb43f0d50a49f6
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-02 18:51:54 +0100

    Prevent orphaned images from user projects

--- project_jotter/projects/models.py
+++ project_jotter/projects/models.py
@@ -6,7 +6,7 @@ from users.models import User
 class Project(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
     name = models.CharField(max_length=150)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank=True, upload_to="project-images/")
     contents = models.JSONField(blank=True, default=list)
     completed = models.BooleanField(blank=True, default=False)
 
@@ -14,6 +14,16 @@ class Project(models.Model):
         super().clean()
         if self.contents is None:
             self.contents = []
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.pk:
+            # If the image has been changed, delete the previous one.
+            old_instance = self.__class__.objects.get(pk=self.pk)
+            if old_instance.image != self.image:
+                old_instance.image.delete(save=False)
+        return super().save(force_insert, force_update, using, update_fields)
 
     def __str__(self):
         return f"{self.name} ({self.author})"
