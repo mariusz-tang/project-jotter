dd6df6ae13018e70dcb14a248f22307267bfcc1c
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-02 18:06:39 +0100

    Change project contents default to an empty list

--- project_jotter/projects/models.py
+++ project_jotter/projects/models.py
@@ -7,8 +7,13 @@ class Project(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
     name = models.CharField(max_length=150)
     image = models.ImageField(blank=True)
-    contents = models.JSONField(blank=True, null=True)
+    contents = models.JSONField(blank=True, default=list)
     completed = models.BooleanField(blank=True, default=False)
+
+    def clean(self) -> None:
+        super().clean()
+        if self.contents is None:
+            self.contents = []
 
     def __str__(self):
         return f"{self.name} ({self.author})"
