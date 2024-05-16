2fa2d904c13a3957d6d65db33479c833c84e9d89
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-16 16:31:23 +0100

    Add edit project details page

--- project_jotter/projects/urls.py
+++ project_jotter/projects/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("create", views.ProjectCreationView.as_view(), name="create-project"),
     path("<username>/<project_name>", views.ProjectDetailView.as_view(), name="view-project"),
+    path("<username>/<project_name>/edit", views.ProjectEditView.as_view(), name="edit-project"),
 ]
--- project_jotter/projects/views.py
+++ project_jotter/projects/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.db.models.base import Model as Model
 from django.http import Http404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
 from django.views import generic
 
 from . import forms
@@ -27,6 +29,44 @@ class ProjectCreationView(LoginRequiredMixin, generic.CreateView):
             self.request, f"Project '{form.instance.name}' created successfully!"
         )
         return super().form_valid(form)
+
+
+class ProjectEditView(generic.UpdateView):
+    template_name = "projects/edit-project-details.html"
+    fields = ["name", "image", "is_private", "is_completed"]
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.warning(request, "Please log in to edit a project.")
+            return redirect("login", next=request.path)
+
+        self.object = self.get_object()
+        if request.user != self.object.author:
+            return redirect(
+                "view-project",
+                username=self.kwargs["username"],
+                project_name=self.kwargs["project_name"],
+            )
+
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None) -> Model:
+        try:
+            return Project.objects.get(
+                author__username=self.kwargs["username"],
+                name=self.kwargs["project_name"],
+            )
+        except Project.DoesNotExist:
+            raise Http404("Project not found.")
+
+    def get_success_url(self):
+        return reverse(
+            "view-project",
+            kwargs={
+                "username": self.kwargs["username"],
+                "project_name": self.object.name,
+            },
+        )
 
 
 class ProjectDetailView(generic.DetailView):
